b438f221ac3c3439f789113e1c986b80c1292c79
JulianGoeltz <julian.goeltz@kip.uni-heidelberg.de>
2025-11-15 23:41:02 +0100

    one more command

--- yalafi/packages/glossaries_extra.py
+++ yalafi/packages/glossaries_extra.py
@@ -32,6 +32,7 @@ def init_module(parser, options, position):
         Macro(parms, '\\glsxtrshort', args='OA', repl=h_gls('short', [])),
         Macro(parms, '\\Glsxtrshort', args='OA',
               repl=h_gls('short', [cap_first])),
+        Macro(parms, '\\glsfmtlongpl', args='OA', repl=h_gls('plural', [])),
         Macro(parms, '\\glsxtrlong', args='OA', repl=h_gls('long', [])),
         Macro(parms, '\\Glsxtrlong', args='OA',
               repl=h_gls('long', [cap_first])),
